b2d0fc86bcbf1ef5ed486e725117a0c099c3d979
rmenner <rmenner@users.noreply.github.com>
2026-02-18 00:06:01 +0000

    Remove preview for PR 23 🛬

--- pr-preview/pr-23/index.js
+++
@@ -1,7 +0,0 @@
-import { AuroTail, AuroTailGroup } from "../src/index";
-
-AuroTail.register();
-AuroTailGroup.register();
-
-AuroTail.register("custom-tail");
-AuroTailGroup.register("custom-tail-group");
