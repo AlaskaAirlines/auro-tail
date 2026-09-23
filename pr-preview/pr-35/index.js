3c3f96aaea1a20389bb1f8a3603b54ce0897c41e
jason-capsule42 <jason-capsule42@users.noreply.github.com>
2026-09-23 19:53:54 +0000

    Deploy preview for PR 35 🛫

---
+++ pr-preview/pr-35/index.js
@@ -0,0 +1,7 @@
+import { AuroTail, AuroTailGroup } from "../src/index";
+
+AuroTail.register();
+AuroTailGroup.register();
+
+AuroTail.register("custom-tail");
+AuroTailGroup.register("custom-tail-group");
